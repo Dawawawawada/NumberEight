dc80f9aa92e74bd6a8f2af4f44990727a1dda49f
Dawawawawada <164107250+Dawawawawada@users.noreply.github.com>
2024-05-27 18:32:30 -0400

    Homework05272024

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@
 
 # Example Usage - Life cycle settings for storage bucket objects
 #BLOCK     #RESOURCE TYPE        #RESOURCE NAME
-resource "google_storage_bucket" "maymadnessmantis-1" {
-  name          = "mantismayhem-1"
+resource "google_storage_bucket" "maymadnessmantis-1a" {
+  name          = "mantismayhem-1a"
   storage_class =  "STANDARD"
   location      = "us-central1"
   force_destroy = true
@@ -27,22 +27,36 @@ resource "google_storage_bucket" "maymadnessmantis-1" {
     not_found_page   = "404.html"
   
   }
+# retention_policy {
+#   retention_period = 1
+#   # #is_locked = false
+# }
 
 }
 resource "google_storage_bucket_object" "photo-1" {
-bucket = google_storage_bucket.maymadnessmantis-1.name
-name = "mantismayhem"
+bucket = google_storage_bucket.maymadnessmantis-1a.name
+name = "mantismayhem.jpg"
 source = "p1023588.jpg"
+content_type = "image/jpeg"
 }
 
 resource "google_storage_bucket_object" "photo-2" {
-name = "mantismayhem2"
+name = "index.html" #name = "p1023588.jpg" 
 source = "index.html"
-bucket = google_storage_bucket.maymadnessmantis-1.name
+content_type = "text/html"
+bucket = google_storage_bucket.maymadnessmantis-1a.name
+# retention {
+#   #retention_period = 30
+#   #is_locked = false
+#   mode = "Unlocked"
+#   retain_until_time = "2025-06-01T00:00:00Z"
+  
+#          }
+
 }
 
 resource "google_storage_bucket_iam_member" "public_access" {
-  bucket = google_storage_bucket.maymadnessmantis-1.name
+  bucket = google_storage_bucket.maymadnessmantis-1a.name
   role = "roles/storage.objectViewer"
   member = "allUsers"
 }
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "capture" {
+  value = "https://storage.googleapis.com/${google_storage_bucket.maymadnessmantis-1a.name}/index.html"
+}
